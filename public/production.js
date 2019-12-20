d8c3aa2051b3db3476b265da131e58288aab28d8
sophy1 <jihye.sophy.won@gmail.com>
2019-12-20 16:05:13 +0900

    Update production.js

--- public/production.js
+++ public/production.js
@@ -1,1 +1,16 @@
-test2
+#!/usr/bin/env node
+const fs = require('fs-extra');
+const path = require('path');
+const os = require('os');
+var child_process = require('child_process');
+
+let folderName = process.argv[2];
+if (folderName) {
+  folderName = folderName.toString();
+} else {
+  console.log(
+    'Please specify a directory name to install frontend starter toolkit in'
+  );
+  process.exit(1);
+}
+fs.ensureDirSync(folderName);
