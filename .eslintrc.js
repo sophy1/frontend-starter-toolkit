14c549f8bc917b9263f5698027e4c911b81715cf
sophy1 <jihye.sophy.won@gmail.com>
2019-12-20 10:25:14 +0900

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:react/recommended',
     
-      "plugin:@typescript-eslint/test-for-not-supported-plugin-config",
+      "plugin:@typescript-eslint/recommended",
       "plugin:@typescript-eslint/eslint-recommended",
       "plugin:@typescript-eslint/recommended-requiring-type-checking",
     'plugin:jsx-a11y/recommended',
